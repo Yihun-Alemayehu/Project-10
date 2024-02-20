b6075ac11bd728371e585872aa5cd1200c1bf32b
Yihun <yankure01@gmail.com>
2024-02-20 11:24:53 +0300

    fdb-05

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_10/bloc/social_media_bloc.dart';
 import 'package:project_10/screens/home_screen.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
@@ -10,8 +12,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return BlocProvider(
+      create: (context) => SocialMediaBloc()..add(FetchPostsEvent()),
+      child: const MaterialApp(
+        home: HomeScreen(),
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_10/bloc/social_media_bloc.dart';
+import 'package:project_10/data/models/post_model.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Posts'),
-        centerTitle: true,
-      ),  
-      body: Column(
-        children: [
-          
-        ],
-      ),
+    return BlocBuilder<SocialMediaBloc, SocialMediaState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Posts'),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: [
+              if (state is SocialMediaLoading)
+                const CircularProgressIndicator()
+              else if (state is SocialMediaLoaded)
+                _buildPostsList(state.posts)
+              else if (state is SocialMediaError)
+                Text(state.errorMessage)
+              else
+                const Text('I don\'t know what happened to you...')
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              context.read<SocialMediaBloc>().add(FetchPostsEvent());
+            },
+            child: const Icon(Icons.refresh),
+          ),
+        );
+      },
     );
   }
+}
+
+Widget _buildPostsList(List<Post> posts) {
+  return Expanded(
+    child: ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(posts[index].username),
+          subtitle: Text(posts[index].content),
+          trailing: Text(posts[index].likes.toString()),
+        );
+      },
+    ),
+  );
 }
